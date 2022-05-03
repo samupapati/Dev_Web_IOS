ac97399c182c8d0f7f263c3af44884b1f6bc143a
samupapati <samuelpapati@gmail.com>
2022-05-03 13:25:49 -0300

    subindo exercicio

---
+++ JavaScript/Aula 03/Funçoes/exercicio/main.js
@@ -0,0 +1,30 @@
+//criando funcao simples
+function alertCookie() {
+    alert("Você quer um cookie?");
+    console.log("Você quer um cookie?");
+    return
+};
+
+//Criando arrow function 
+const alertSucesso = () => {
+    alert("Mensagem gerada com sucesso!");
+    console.log("Mensagem gerada com sucesso!");
+};
+
+//Criando objeto notebook
+const notebook = {
+    nome: 'M15',
+    modelo: 'Gamer',
+    marca: 'Alienware',
+    cor: 'Cinza escuro',
+    valor: 11.700
+};
+//Exibindo o objeto notebook no console
+console.log(notebook);
+
+
+const ConverterAnosEmDias = (anos = 2022, dias = 365) => {
+    return anos * dias; 
+};
+console.log(ConverterAnosEmDias);
+
