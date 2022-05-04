811420f31eb7c51c5ad9975f72900a7fbb1cbf38
samuel <samuelpapati@gmail.com>
2022-05-03 21:35:56 -0300

    subindo exercicio

---
+++ JavaScript/Aula 04 - Arrays/exercicio/main.js
@@ -0,0 +1,43 @@
+const pessoas = ['Joaquim', 'José', 'Silva', 'Xavier'];
+console.log(`Numa festa haviam 4 pessoas: `, pessoas);
+
+
+console.log(`Chegou amanda, saiu Xavier`);
+pessoas.unshift('Amanda');
+var saiu1 = pessoas.pop();
+console.log(pessoas);
+
+
+console.log(`Chegou Zuleica, saiu Amanda`);
+pessoas.push('Zuleica');
+saiu2 = pessoas.shift();
+console.log(pessoas);
+
+
+console.log(`Saiu Silva, chegou Silvana`);
+delete pessoas[2];
+pessoas[2] = "Silvana";
+console.log(pessoas);
+
+var pessoasString = pessoas.join(' / ');
+console.log(`Ficaram 4 pessoas na festa: ${pessoasString}`);
+
+
+
+
+
+
+var carros = ['Vectra', 'Lancer', 'Golf_GTI', 'Santa_Fé', 'Stilo'];
+//Adicionando corolla e hb20
+carros.splice(7, 8, "Corolla", "HB20");
+console.log(carros);
+//criando novo array com os elementos da variavel carros
+carros2 = carros.slice(5);
+console.log(carros2);
+
+//transformando variavel carros em string
+var carrosString = carros.join(' / ');
+console.log(`Antes:`,carros);
+console.log(`Depois:`,carrosString);
+
+
