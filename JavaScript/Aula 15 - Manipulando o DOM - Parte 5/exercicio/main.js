f565bae3029dd8035404e1966c8525cb413b1008
samupapati <samuelpapati@gmail.com>
2022-05-26 21:44:18 -0300

    subindo exercicio

--- JavaScript/Aula 15 - Manipulando o DOM - Parte 5/exercicio/main.js
+++ JavaScript/Aula 15 - Manipulando o DOM - Parte 5/exercicio/main.js
@@ -4,15 +4,7 @@ itemAdicionar = document.querySelector('.itemAdicionar');
 valorAdicionar = document.querySelector('.valorAdicionar');
 quantidadeAdicionar = document.querySelector('.quantidadeAdicionar');
 
-divLi = document.querySelector('.divLi');
-li = document.querySelector('.li');
-valores = document.querySelector('.valores');
-cifrao = document.querySelector('.cifrao');
-buttonsItem = document.querySelector('.buttonsItem');
-aumentarQtd = document.querySelector('.aumentarQtd');
-numeroQtd = document.querySelector('.numeroQtd');
-diminuirQtd = document.querySelector('.diminuirQtd');
-excluirItem = document.querySelector('.excluirItem');
+
 
 subtotal = document.querySelector('.subtotal');
 liSubtotal = document.querySelector('.liSubtotal');
@@ -24,6 +16,6 @@ adicionarItem.addEventListener('submit', adicionarAoCarrinho);
 function adicionarAoCarrinho(e){
   e.preventDefault();
   if(itemAdicionar.value != '' && valorAdicionar.value != '' && quantidadeAdicionar.value != ''){
-    alert('teste')
+
   }
 }
