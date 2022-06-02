cc1c0768a9c84d43e3c6a19ec8ed76069d561aed
samupapati <samuelpapati@gmail.com>
2022-06-01 21:47:47 -0300

    subindo aula 03

---
+++ React/Aula 03 - Componentes Funcionais/aula_03/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Exemplo from './Components/Exemplo';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header nome="Samuel" />
+      <Exemplo />
+    </div>
+  );
+}
+
+export default App; 
