e5b518749d46f3f38641639b7fc31be316f8a1ae
samupapati <samuelpapati@gmail.com>
2022-06-02 19:14:15 -0300

    subindo exercicio 13

--- JavaScript/Aula 13 - JavaScript HTML DOM – Parte 03/exercicio/main.js
+++ JavaScript/Aula 13 - JavaScript HTML DOM – Parte 03/exercicio/main.js
@@ -34,35 +34,35 @@ let botao1 = document.querySelector('#B_01');
 let botao2 = document.querySelector('#B_02');
 let botao3 = document.querySelector('#B_03');
 
-//Novo botão para remover os conteúdos
+//
 let botaoRemover = document.querySelector('.remove')
 botaoRemover.innerHTML = 'Remover conteudo'
 botaoRemover.addEventListener('click', removerConteudo)
 
+
 //Configurando para quando o botão ser clicado chamar a função e desabilitar após clicado uma vez
 botao1.addEventListener('click', mostrarUmbrellaAcademy)
+botao1.addEventListener('click', function(){ //função para desabilitar o botão após o usuário clicar
+    this.disabled = true; //desabilita o botao
+}) //Depois do usuário clicar o botão é desabilitado para não ficar clicando várias vezes
 
 botao2.addEventListener('click', receberNomeUsuario);
 
 botao3.addEventListener('click', tabuadaNumeroInformado);
-//botão 3 não tem restrição de cliques
+
 
 //============================================================================================================
-imgUA = document.createElement('img'); //criando tag img
+
 //Criando as funções
 function mostrarUmbrellaAcademy(){
     imgUA = document.createElement('img'); //criando tag img
     imgUA.src = 'imgs/UA.jpg'; //buscando imagem
     imgUA.style.width = '60%'; 
+    imgUA.className = 'excluir'
     document.body.insertBefore(imgUA, botao1.nextElementSibling); //inserindo a imagem depois do botao1
     document.body.style.backgroundColor = '#262525';
-
-    botaoRemover.style.display = 'block'
+    botaoRemover.style.display = 'block';
 }
-
-divP1 = document.createElement('div')
-document.body.insertBefore(divP1, botao2.nextElementSibling)
-paragrafo = document.createElement('p');
 
 function receberNomeUsuario(){ 
     paragrafo = document.createElement('p');
@@ -72,12 +72,10 @@ function receberNomeUsuario(){
     paragrafo.style.color = 'white';
     paragrafo.style.fontFamily = 'calibri';
     paragrafo.style.fontWeight = 'bold'; //inserindo tag p depois do botao 2
-    divP1.appendChild(paragrafo)
+    paragrafo.className = 'excluir'
+    document.body.insertBefore(paragrafo, botao2.nextElementSibling); //inserindo a imagem depois do botao1
     botaoRemover.style.display = 'block'
 }
-
-divp = document.createElement('div')
-document.body.appendChild(divp)
 
 function tabuadaNumeroInformado(){
     let numeroInformado = window.prompt('Por favor, digite um número'); //perguntando ao usuário para digitar um número e guardando na variável numeroInformado
@@ -89,27 +87,31 @@ function tabuadaNumeroInformado(){
         tituloTabuada = document.createElement('h3'); //criando h3
         tituloTabuada.innerHTML = `A tabuada do número ${numeroInformado} é:`; //inserindo conteúdo no h3
         tituloTabuada.style.color = 'white';
+        tituloTabuada.className = 'excluir'
         tituloTabuada.style.fontFamily = 'calibri';
-        divp.appendChild(tituloTabuada); // inserindo h3 depois do botao3
+        document.body.insertBefore(tituloTabuada, botao3.nextElementSibling); //inserindo a imagem depois do botao1
         for(let i = 0; i <= 10; i++){ // loop para fazer a tabuada
             paragrafo2 = document.createElement('p'); //criando tag p 2
             paragrafo2.innerHTML = `${numeroInformado} x ${i} = ${numeroInformado * i}`; //inserindo conteúdo na tag p   
             paragrafo2.style.color = 'white';
             paragrafo2.style.fontFamily = 'calibri';
             paragrafo2.style.fontWeight = 'bold';
-            paragrafo2.className = 'pTabuada'
-            divp.appendChild(paragrafo2); //inserindo tag p depois do titulo h3
+            paragrafo2.className = 'excluir'
+            document.body.insertBefore(paragrafo2, tituloTabuada.nextElementSibling); //inserindo tag p depois do titulo h3
         
             botaoRemover.style.display = 'block'
         }
     }
 }
+
 function removerConteudo(){
-    divp.remove()
-    imgUA.remove()
-    paragrafo.remove()
-    divP1.removeChild(paragrafo)
+    var lista = document.getElementsByClassName("excluir");
+    for(var i = lista.length - 1; i >= 0; i--)
+    {
+        lista[i].remove()
+    }
     document.body.style.backgroundColor = '#BFBFBF'
+    botao1.disabled = false; //habilita o botao
 }
 
 //============================================================================================================
