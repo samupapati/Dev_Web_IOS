f881234faac82079060e0ff95594dea3fcc6a5dd
samupapati <samuelpapati@gmail.com>
2022-06-24 18:32:35 -0300

    subindo aula

--- Java/Aula 03 - Desvios condicionais/Exercicio/src/Exercicio.java
+++ Java/Aula 03 - Desvios condicionais/Exercicio/src/Exercicio.java
@@ -12,17 +12,27 @@ public class Exercicio {
         if(mes.equalsIgnoreCase("janeiro")){
             System.out.println("A estação de " + mes + " é " + " Verão.");
         } else if(mes.equalsIgnoreCase("fevereiro")){
-            System.out.println("");
-        } else if(mes.equalsIgnoreCase("")){
-            System.out.println("");
-        } else if(mes.equalsIgnoreCase("")){
-            System.out.println("");
-        } else if(mes.equalsIgnoreCase("")){
-            System.out.println("");
-        } else if(mes.equalsIgnoreCase("")){
-            System.out.println("");
-        } else if(mes.equalsIgnoreCase("")){
-            System.out.println("");
+            System.out.println("A estação de " + mes + " é " + " Verão.");
+        } else if(mes.equalsIgnoreCase("março")){
+            System.out.println("A estação de " + mes + " é " + " Verão.");
+        } else if(mes.equalsIgnoreCase("abril")){
+            System.out.println("A estação de " + mes + " é " + " Outono.");
+        } else if(mes.equalsIgnoreCase("maio")){
+            System.out.println("A estação de " + mes + " é " + " Outono.");
+        } else if(mes.equalsIgnoreCase("junho")){
+            System.out.println("A estação de " + mes + " é " + " Outono.");
+        } else if(mes.equalsIgnoreCase("julho")){
+            System.out.println("A estação de " + mes + " é " + " Inverno.");
+        } else if(mes.equalsIgnoreCase("agosto")){
+            System.out.println("A estação de " + mes + " é " + " Inverno.");
+        } else if(mes.equalsIgnoreCase("setembro")){
+            System.out.println("A estação de " + mes + " é " + " Inverno.");
+        } else if(mes.equalsIgnoreCase("outubro")){
+            System.out.println("A estação de " + mes + " é " + " Primavera.");
+        } else if(mes.equalsIgnoreCase("novembro")){
+            System.out.println("A estação de " + mes + " é " + " Primavera.");
+        } else if(mes.equalsIgnoreCase("dezembro")){
+            System.out.println("A estação de " + mes + " é " + " Primavera.");
         }
     }
 }
